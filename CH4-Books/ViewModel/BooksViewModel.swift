7b2caf1d181ba089710ad95a4d98c211aa5f7838
MrSampaio <jusampa2@gmail.com>
2026-08-13 16:39:19 -0300

    feat: implements CoreDataManager class

---
+++ CH4-Books/ViewModel/BooksViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  BooksViewModel.swift
+//  CH4-Books
+//
+//  Created by Julio Sampaio on 13/08/26.
+//
+
+import Foundation
---
+++ CH4-Books/ViewModel/Managers/CoreDataManager.swift
@@ -0,0 +1,23 @@
+//
+//  CoreDataManager.swift
+//  CH4-Books
+//
+//  Created by Julio Sampaio on 13/08/26.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager{
+    let persistentContainer: NSPersistentContainer
+    static let shared = CoreDataManager()
+    
+    init(){
+        self.persistentContainer = NSPersistentContainer(name: "Database")
+        self.persistentContainer.loadPersistentStores { (description, error) in
+            if let error = error{
+                fatalError("Error loading persistent stores: \(error)")
+            }
+        }
+    }
+}
