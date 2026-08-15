c20d6a913fe27fac15db04623107bc8acb233661
lucas <pinheirolucas345543@gmail.com>
2026-08-15 09:11:10 -0300

    feat: adding a function to format time

--- Flush/ViewModel/StopwatchViewModel.swift
+++ Flush/ViewModel/StopwatchViewModel.swift
@@ -11,24 +11,21 @@ class StopwatchViewModel: ObservableObject {
     @Published var elapsedTime: TimeInterval = 120
     @Published var isRunning: Bool = false
     @Published var timer: Timer? = nil
-    var timeString: String {
-        let minutes = Int(elapsedTime) / 60
-        let seconds = Int(elapsedTime) % 60
-        return String(format: "%02d:%02d", minutes, seconds)
-    }
-    
+        
     func start() {
         timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
             self.elapsedTime -= 0.1
-            
-            
         }
-        
     }
+    
     func stop() {
         isRunning = false
         timer?.invalidate()
         timer = nil
+    }
+    
+    func timerFormater() -> String{
+        return String(format: "%02d:%02d", Int(elapsedTime) / 60, Int(elapsedTime) % 60)
     }
     
 }
