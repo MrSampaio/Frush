55bb63f2254774ffcf63931a94d9c22bb33bd1c4
MrSampaio <jusampa2@gmail.com>
2026-08-18 22:51:30 -0300

    feat: implements sheet and toolbar into BookDetailsView

--- Flush/View/BookDetailsView.swift
+++ Flush/View/BookDetailsView.swift
@@ -12,6 +12,7 @@ struct BookDetailView: View {
     @ObservedObject var viewModel: BooksViewModel
     @StateObject private var notesViewModel = NotesViewModel()
     @State private var isPresentedAddNote: Bool = false
+    @State private var isPresentedEditBook: Bool = false
     @EnvironmentObject var photoLibraryViewModel: PhotoLibraryViewModel
     
     var book: Books? = nil
@@ -21,100 +22,98 @@ struct BookDetailView: View {
     }
     
     var body: some View {
-        ZStack {
-            Color(.orange)
-                .opacity(0.1)
-                .ignoresSafeArea()
-            
-            if let currentBook = currentBook {
-                ScrollView(showsIndicators: false) {
-                    VStack(spacing: 24) {
-                        
-                        HStack {
-                            Spacer()
-                            Button(action: {
-                            }) {
-                                Image(systemName: "pencil")
-                                    .font(.system(size: 16, weight: .bold))
-                                    .foregroundColor(.white)
-                                    .frame(width: 44, height: 44)
-                                    .background(Color(.orange))
-                                    .clipShape(Circle())
-                            }
-                        }
-                        .padding(.horizontal)
-                        .padding(.top, 16)
-                        
-                        
-                        BookInstanceDetailView(book: currentBook)
-                        
-                        CardTotalPages(totalPages: 100)
-                        .padding(.horizontal)
-                        
-                        VStack(spacing: 16) {
-                            NotesHeaderview(isPresentedAddNote: $isPresentedAddNote)
-                            NotesSectionView(notes: notesViewModel.savedNotes)
-                        }
-                        .padding(.horizontal)
-                        
-                        VStack(spacing: 16) {
-                            Button(action: {
-                            }) {
-                                Text("Adicionar leitura")
-                                    .font(.system(size: 16, weight: .semibold))
-                                    .foregroundColor(.white)
-                                    .frame(maxWidth: .infinity)
-                                    .padding(.vertical, 16)
-//                                    .background(
-//                                        RoundedRectangle(cornerRadius: 24)
-//                                    )
-//                                    .overlay(
-//                                        RoundedRectangle(cornerRadius: 24)
-//                                            .stroke(Color.white.opacity(0.1), lineWidth: 1)
-//                                    )
-                            }
-                            .buttonStyle(.glass)
-                            .padding(.horizontal, 24)
-                            .padding(.top, 16)
+        NavigationStack{
+            ZStack {
+                Color(.orange)
+                    .opacity(0.1)
+                    .ignoresSafeArea()
+                
+                if let currentBook = currentBook {
+                    ScrollView(showsIndicators: false) {
+                        VStack(spacing: 24) {
                             
-                            Button(action: {
-                            }) {
-                                Text("Iniciar leitura")
-                                    .font(.system(size: 16, weight: .semibold))
-                                    .foregroundColor(.white)
-                                    .frame(maxWidth: .infinity)
-                                    .padding(.vertical, 16)
-                                   
-//
-                                    .cornerRadius(24)
+                            BookInstanceDetailView(book: currentBook)
+                            
+                            CardTotalPages(totalPages: 100)
+                            .padding(.horizontal)
+                            
+                            VStack(spacing: 16) {
+                                NotesHeaderview(isPresentedAddNote: $isPresentedAddNote)
+                                NotesSectionView(notes: notesViewModel.savedNotes)
                             }
-                            //.background(Color(.action))
-                            .buttonStyle(.glass)
-                            .padding(.horizontal, 24)
-                            .padding(.top, 16)
+                            .padding(.horizontal)
+                            
+                            VStack(spacing: 16) {
+                                Button(action: {
+                                }) {
+                                    Text("Adicionar leitura")
+                                        .font(.system(size: 16, weight: .semibold))
+                                        .foregroundColor(.white)
+                                        .frame(maxWidth: .infinity)
+                                        .padding(.vertical, 16)
+    //                                    .background(
+    //                                        RoundedRectangle(cornerRadius: 24)
+    //                                    )
+    //                                    .overlay(
+    //                                        RoundedRectangle(cornerRadius: 24)
+    //                                            .stroke(Color.white.opacity(0.1), lineWidth: 1)
+    //                                    )
+                                }
+                                .buttonStyle(.glass)
+                                .padding(.horizontal, 24)
+                                .padding(.top, 16)
+                                
+                                Button(action: {
+                                }) {
+                                    Text("Iniciar leitura")
+                                        .font(.system(size: 16, weight: .semibold))
+                                        .foregroundColor(.white)
+                                        .frame(maxWidth: .infinity)
+                                        .padding(.vertical, 16)
+                                       
+    //
+                                        .cornerRadius(24)
+                                }
+                                //.background(Color(.action))
+                                .buttonStyle(.glass)
+                                .padding(.horizontal, 24)
+                                .padding(.top, 16)
+                                
+                            }
+           
                             
                         }
-       
-                        
+                        .padding(.bottom, 40)
                     }
-                    .padding(.bottom, 40)
+                } else {
+                    Text("Nenhum livro encontrado.")
+                        .foregroundColor(.secondary)
                 }
-            } else {
-                Text("Nenhum livro encontrado.")
-                    .foregroundColor(.secondary)
+            }
+            .onAppear {
+                notesViewModel.fetchNotes(for: currentBook)
+            }
+            .sheet(isPresented: $isPresentedAddNote, onDismiss: {
+                notesViewModel.fetchNotes(for: currentBook)
+            }) {
+                if let currentBook {
+                    NoteSheetView(book: currentBook)
+                        .environmentObject(notesViewModel)
+                }
+            }
+            .sheet(isPresented: $isPresentedEditBook){
+                BookSheetView()
+                    .environmentObject(PhotoLibraryViewModel())
+                    .environmentObject(BooksViewModel())
+            }
+            .toolbar{
+                BooksDetailsToolbar(onEdit: {
+                    isPresentedEditBook.toggle()
+                    
+                })
             }
         }
-        .onAppear {
-            notesViewModel.fetchNotes(for: currentBook)
-        }
-        .sheet(isPresented: $isPresentedAddNote, onDismiss: {
-            notesViewModel.fetchNotes(for: currentBook)
-        }) {
-            if let currentBook {
-                NoteSheetView(book: currentBook)
-                    .environmentObject(notesViewModel)
-            }
-        }
+
     }
 }
 #Preview {
